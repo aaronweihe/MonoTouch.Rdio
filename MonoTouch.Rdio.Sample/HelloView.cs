3062b575e220b7ce1d1f8f2a8faa28ded2c0cbb2
Aaron He <ahe@ain1.com>
2013-03-22 16:57:46 -0700

    Recover from accidental deletion of a *.designer.cs file

--- MonoTouch.Rdio.Sample/HelloView.cs
+++ MonoTouch.Rdio.Sample/HelloView.cs
@@ -7,64 +7,64 @@ using MonoTouch.Rdio;
 
 namespace MonoTouch.Rdio.Sample
 {
-    public partial class HelloView : UIViewController
-    {
-        public bool IsLoggedIn = false;
-        public Rdio MyRdio = new Rdio();
-
-        public HelloView()
-            : base("HelloView", null)
-        {
-        }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-
-            // Perform any additional setup after loading the view, typically from a nib.
-            btnLogin.TouchDown += (sender, e) =>
-            {
-                var del = new MyRdioDelegate(this);
-                //Initialize Rdio with your consumer key and secret
-                MyRdio.InitWithConsumerKeyandSecretdelegate("", "", del);
-
-                MyRdio.AuthorizeFromController(this);
-
-            };
-
-            btnPlay.TouchDown += (sender, e) =>
-                {
-                    if (IsLoggedIn) {
-                        MyRdio.Player.PlaySource("t2742133");
-                    }
-                };
-        }
-
-    }
-
-    class MyRdioDelegate : RdioDelegate
-    {
-        private readonly HelloView _view;
-
-        public MyRdioDelegate(HelloView view)
-        {
-            _view = view;
-        }
-
-        public override void RdioAuthorizationCancelled()
-        {
-            _view.IsLoggedIn = false;
-        }
-
-        public override void RdioAuthorizationFailed(string error)
-        {
-            _view.IsLoggedIn = false;
-        }
-
-        public override void RdioDidAuthorizeUserwithAccessToken(NSDictionary user, string accessToken)
-        {
-            _view.IsLoggedIn = true;
-        }
-    }
+	public partial class HelloView : UIViewController
+	{
+		public bool IsLoggedIn = false;
+		public Rdio MyRdio = new Rdio();
+		
+		public HelloView()
+			: base("HelloView", null)
+		{
+		}
+		
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			btnLogin.TouchDown += (sender, e) =>
+			{
+				var del = new MyRdioDelegate(this);
+				//Initialize Rdio with your consumer key and secret
+				MyRdio.InitWithConsumerKeyandSecretdelegate("", "", del);
+				
+				MyRdio.AuthorizeFromController(this);
+				
+			};
+			
+			btnPlay.TouchDown += (sender, e) =>
+			{
+				if (IsLoggedIn) {
+					MyRdio.Player.PlaySource("t2742133");
+				}
+			};
+		}
+		
+	}
+	
+	class MyRdioDelegate : RdioDelegate
+	{
+		private readonly HelloView _view;
+		
+		public MyRdioDelegate(HelloView view)
+		{
+			_view = view;
+		}
+		
+		public override void RdioAuthorizationCancelled()
+		{
+			_view.IsLoggedIn = false;
+		}
+		
+		public override void RdioAuthorizationFailed(string error)
+		{
+			_view.IsLoggedIn = false;
+		}
+		
+		public override void RdioDidAuthorizeUserwithAccessToken(NSDictionary user, string accessToken)
+		{
+			_view.IsLoggedIn = true;
+		}
+	}
 }
 
---
+++ MonoTouch.Rdio.Sample/HelloView.designer.cs
@@ -0,0 +1,33 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace MonoTouch.Rdio.Sample
+{
+	[Register ("HelloView")]
+	partial class HelloView
+	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnPlay { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnLogin { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnPlay != null) {
+				btnPlay.Dispose ();
+				btnPlay = null;
+			}
+
+			if (btnLogin != null) {
+				btnLogin.Dispose ();
+				btnLogin = null;
+			}
+		}
+	}
+}
