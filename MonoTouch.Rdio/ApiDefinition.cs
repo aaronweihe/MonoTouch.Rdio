681031c257500e659c9bd0c7697edc386a6921a2
Aaron He <ahe@ain1.com>
2013-11-07 15:43:15 -0800

    Format documemt

--- MonoTouch.Rdio/ApiDefinition.cs
+++ MonoTouch.Rdio/ApiDefinition.cs
@@ -1,16 +1,15 @@
 using System;
-
 using MonoTouch.ObjCRuntime;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
-namespace MonoTouch.Rdio {
-
-	[BaseType (typeof (NSObject), Name = "RDAPIRequestDelegate")]
+namespace MonoTouch.Rdio
+{
+	[BaseType (typeof(NSObject), Name = "RDAPIRequestDelegate")]
 	[Model]
 	[Protocol]
-	public partial interface RDAPIRequestDelegateHandlers {
-
+	public partial interface RDAPIRequestDelegateHandlers
+	{
 		[Export ("rdioRequest:didLoadData:")]
 		void DidLoadData (RDAPIRequest request, NSObject data);
 
@@ -18,9 +17,9 @@ namespace MonoTouch.Rdio {
 		void DidFailWithError (RDAPIRequest request, NSError error);
 	}
 
-	[BaseType (typeof (NSObject), Name = "RDAPIRequestDelegate")]
-	public partial interface RDAPIRequestDelegate : RDAPIRequestDelegateHandlers {
-
+	[BaseType (typeof(NSObject), Name = "RDAPIRequestDelegate")]
+	public partial interface RDAPIRequestDelegate : RDAPIRequestDelegateHandlers
+	{
 		[Static, Export ("delegateToTarget:loadedAction:failedAction:")]
 		NSObject DelegateToTarget (NSObject target, Selector load, Selector fail);
 
@@ -34,9 +33,9 @@ namespace MonoTouch.Rdio {
 		NSObject UserInfo { get; set; }
 	}
 
-	[BaseType (typeof (NSObject), Name = "RDAPIRequest")]
-	public partial interface RDAPIRequest {
-
+	[BaseType (typeof(NSObject), Name = "RDAPIRequest")]
+	public partial interface RDAPIRequest
+	{
 		[Export ("cancel")]
 		void Cancel ();
 
@@ -44,13 +43,13 @@ namespace MonoTouch.Rdio {
 		NSDictionary Parameters { get; }
 	}
 
-	[BaseType (typeof (NSObject), Name = "RDPlayerDelegate")]
+	[BaseType (typeof(NSObject), Name = "RDPlayerDelegate")]
 	[Model]
 	[Protocol]
-	public partial interface RDPlayerDelegate {
-
+	public partial interface RDPlayerDelegate
+	{
 		[Export ("rdioIsPlayingElsewhere")]
-		bool RdioIsPlayingElsewhere();
+		bool RdioIsPlayingElsewhere ();
 
 		[Export ("rdioPlayerChangedFromState:toState:")]
 		void ToState (RDPlayerState oldState, RDPlayerState newState);
@@ -59,17 +58,17 @@ namespace MonoTouch.Rdio {
 		void RdioPlayerQueueDidChange ();
 
 		[Export ("rdioPlayerCouldNotStreamTrack:")]
-		bool RdioCouldNotStreamTrack(string trackKey);
+		bool RdioCouldNotStreamTrack (string trackKey);
 	}
 
-	[BaseType (typeof (NSObject), Name = "RDPlayer")]
-	public partial interface RDPlayer {
-
+	[BaseType (typeof(NSObject), Name = "RDPlayer")]
+	public partial interface RDPlayer
+	{
 		[Export ("playSource:")]
 		void PlaySource (string sourceKey);
 
 		[Export ("playSources:")]
-		void PlaySources (string [] sourceKeys);
+		void PlaySources (string[] sourceKeys);
 
 		[Export ("next")]
 		void Next ();
@@ -99,88 +98,88 @@ namespace MonoTouch.Rdio {
 		void QueueSource (string sourceKey);
 
 		[Export ("queueSources:")]
-		void QueueSources (string [] sourceKeys);
+		void QueueSources (string[] sourceKeys);
 
-			[Export ("updateQueue:withCurrentTrackIndex:")]
-			bool UpdateQueue (string[] sourceKeys, int index);
+		[Export ("updateQueue:withCurrentTrackIndex:")]
+		bool UpdateQueue (string[] sourceKeys, int index);
 
-			[Export ("resetQueue")]
-			void ResetQueue ();
+		[Export ("resetQueue")]
+		void ResetQueue ();
 
-			[Export ("state")]
-			RDPlayerState State { get; }
+		[Export ("state")]
+		RDPlayerState State { get; }
 
-			[Export ("position")]
-			double Position { get; }
+		[Export ("position")]
+		double Position { get; }
 
-			[Export ("duration")]
-			double Duration { get; }
+		[Export ("duration")]
+		double Duration { get; }
 
-			[Export ("currentTrack")]
-			string CurrentTrack { get; }
+		[Export ("currentTrack")]
+		string CurrentTrack { get; }
 
-			[Export ("currentTrackIndex")]
-			int CurrentTrackIndex { get; }
+		[Export ("currentTrackIndex")]
+		int CurrentTrackIndex { get; }
 
-			[Export ("trackKeys")]
-			NSObject [] TrackKeys { get; }
+		[Export ("trackKeys")]
+		NSObject [] TrackKeys { get; }
 
-			[Wrap ("WeakDelegate")][NullAllowed]
-			RDPlayerDelegate Delegate{get;set;}
+		[Wrap ("WeakDelegate")][NullAllowed]
+		RDPlayerDelegate Delegate{ get; set; }
 
-			[Export ("delegate", ArgumentSemantic.Assign)][NullAllowed]
-			NSObject WeakDelegate { get; set; }
-			}
+		[Export ("delegate", ArgumentSemantic.Assign)][NullAllowed]
+		NSObject WeakDelegate { get; set; }
+	}
 
-			[BaseType (typeof (NSObject))]
-			public partial interface Rdio {
+	[BaseType (typeof(NSObject))]
+	public partial interface Rdio
+	{
+		[Export ("initWithConsumerKey:andSecret:delegate:")]
+		IntPtr Constructor (string key, string secret, [NullAllowed] RdioDelegate aDelegate);
 
-				[Export ("initWithConsumerKey:andSecret:delegate:")]
-				IntPtr Constructor (string key, string secret, [NullAllowed] RdioDelegate aDelegate);
+		[Export ("authorizeFromController:")]
+		void AuthorizeFromController (UIViewController currentController);
 
-				[Export ("authorizeFromController:")]
-				void AuthorizeFromController (UIViewController currentController);
+		[Export ("authorizeUsingAccessToken:")]
+		void AuthorizeUsingAccessToken (string accessToken);
 
-				[Export ("authorizeUsingAccessToken:")]
-				void AuthorizeUsingAccessToken (string accessToken);
+		[Export ("authorizeUsingAccessToken:fromController:")]
+		void AuthorizeUsingAccessToken (string accessToken, UIViewController currentController);
 
-				[Export ("authorizeUsingAccessToken:fromController:")]
-				void AuthorizeUsingAccessToken (string accessToken, UIViewController currentController);
+		[Export ("logout")]
+		void Logout ();
 
-				[Export ("logout")]
-				void Logout ();
+		[Export ("callAPIMethod:withParameters:delegate:")]
+		RDAPIRequest CallAPIMethod (string method, NSDictionary parameters, RDAPIRequestDelegate aDelegate);
 
-				[Export ("callAPIMethod:withParameters:delegate:")]
-				RDAPIRequest CallAPIMethod (string method, NSDictionary parameters, RDAPIRequestDelegate aDelegate);
+		[Wrap ("WeakDelegate")][NullAllowed]
+		RdioDelegate Delegate{ get; set; }
 
-				[Wrap("WeakDelegate")][NullAllowed]
-				RdioDelegate Delegate{get;set;}
+		[Export ("delegate", ArgumentSemantic.Assign)][NullAllowed]
+		NSObject WeakDelegate { get; set; }
 
-				[Export ("delegate", ArgumentSemantic.Assign)][NullAllowed]
-				NSObject WeakDelegate { get; set; }
+		[Export ("user")]
+		NSDictionary User { get; }
 
-				[Export ("user")]
-				NSDictionary User { get; }
+		[Export ("player")]
+		RDPlayer Player { get; }
+	}
 
-				[Export ("player")]
-				RDPlayer Player { get; }
-			}
+	[BaseType (typeof(NSObject))]
+	[Model]
+	[Protocol]
+	public partial interface RdioDelegate
+	{
+		[Export ("rdioDidAuthorizeUser:withAccessToken:")]
+		void WithAccessToken (NSDictionary user, string accessToken);
 
-			[BaseType (typeof (NSObject))]
-			[Model]
-			[Protocol]
-			public partial interface RdioDelegate {
+		[Export ("rdioAuthorizationFailed:")]
+		void AuthorizationFailed (string error);
 
-				[Export ("rdioDidAuthorizeUser:withAccessToken:")]
-				void WithAccessToken (NSDictionary user, string accessToken);
+		[Export ("rdioAuthorizationCancelled")]
+		void RdioAuthorizationCancelled ();
 
-				[Export ("rdioAuthorizationFailed:")]
-				void AuthorizationFailed (string error);
-
-				[Export ("rdioAuthorizationCancelled")]
-				void RdioAuthorizationCancelled ();
-
-				[Export ("rdioDidLogout")]
-				void RdioDidLogout ();
+		[Export ("rdioDidLogout")]
+		void RdioDidLogout ();
 	}
 }
